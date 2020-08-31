0c2033ac4bfe1d9b0cde229785af3f1caef32f07
Jongkil Park <park108@gmail.com>
2020-08-31 15:36:23 +0900

    Initial Commit 2
    새로 생성한 소스로 업데이트

--- src/main/java/ohcna/BookingCreated.java
+++ src/main/java/ohcna/BookingCreated.java
@@ -1,4 +1,3 @@
-
 package ohcna;
 
 public class BookingCreated extends AbstractEvent {
---
+++ src/main/java/ohcna/BookingListViewHandler.java
@@ -0,0 +1,52 @@
+package ohcna;
+
+import ohcna.config.kafka.KafkaProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookingListViewHandler {
+
+
+    @Autowired
+    private BookingListRepository bookingListRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenBookingCreated_then_CREATE_1 (@Payload BookingCreated bookingCreated) {
+        try {
+            if (bookingCreated.isMe()) {
+                // view 객체 생성
+                BookingList bookingList = new BookingList();
+                // view 객체에 이벤트의 Value 를 set 함
+                bookingList.setBookingId(bookingCreated.getId());
+                bookingList.setRoomId(bookingCreated.getRoomId());
+                bookingList.setUseStartDtm(bookingCreated.getUseStartDtm());
+                bookingList.setUseEndDtm(bookingCreated.getUseEndDtm());
+                bookingList.setBookingUserId(bookingCreated.getBookingUserId());
+                // view 레파지 토리에 save
+                bookingListRepository.save(bookingList);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenConfirmRequested_then_UPDATE_1(@Payload ConfirmRequested confirmRequested) {
+        try {
+            if (confirmRequested.isMe()) {
+                // view 객체 조회
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
